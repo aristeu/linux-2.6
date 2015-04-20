9848ba8a38d0cf71b22fe6fb54299c313864c07d
Aristeu Rozanski <arozansk@redhat.com>
2015-04-20 16:13:59 -0400

    ipv4: igmp - add stub for ip_mc_rejoin_groups()
    One less ifdef in the middle of the code.
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

--- net/ipv4/igmp.c
+++ net/ipv4/igmp.c
@@ -1212,9 +1212,38 @@ static inline void igmp_mc_group_added(struct ip_mc_list *im)
 	im->crcount = in_dev->mr_qrv ?: sysctl_igmp_qrv;
 	igmp_ifc_event(in_dev);
 }
+
+/*
+ *	Resend IGMP JOIN report; used by netdev notifier.
+ */
+static void ip_mc_rejoin_groups(struct in_device *in_dev)
+{
+	struct ip_mc_list *im;
+	int type;
+
+	ASSERT_RTNL();
+
+	for_each_pmc_rtnl(in_dev, im) {
+		if (im->multiaddr == IGMP_ALL_HOSTS)
+			continue;
+
+		/* a failover is happening and switches
+		 * must be notified immediately
+		 */
+		if (IGMP_V1_SEEN(in_dev))
+			type = IGMP_HOST_MEMBERSHIP_REPORT;
+		else if (IGMP_V2_SEEN(in_dev))
+			type = IGMPV2_HOST_MEMBERSHIP_REPORT;
+		else
+			type = IGMPV3_HOST_MEMBERSHIP_REPORT;
+		igmp_send_report(in_dev, im, type);
+	}
+}
+
 #else
 static inline void igmp_mc_group_dropped(struct ip_mc_list *im) {}
 static inline void igmp_mc_group_added(struct ip_mc_list *im) {}
+static inline void ip_mc_rejoin_groups(struct in_device *in_dev) {}
 #endif
 
 static void igmp_group_dropped(struct ip_mc_list *im)
@@ -1348,35 +1377,6 @@ out:
 	return;
 }
 EXPORT_SYMBOL(ip_mc_inc_group);
-
-/*
- *	Resend IGMP JOIN report; used by netdev notifier.
- */
-static void ip_mc_rejoin_groups(struct in_device *in_dev)
-{
-#ifdef CONFIG_IP_MULTICAST
-	struct ip_mc_list *im;
-	int type;
-
-	ASSERT_RTNL();
-
-	for_each_pmc_rtnl(in_dev, im) {
-		if (im->multiaddr == IGMP_ALL_HOSTS)
-			continue;
-
-		/* a failover is happening and switches
-		 * must be notified immediately
-		 */
-		if (IGMP_V1_SEEN(in_dev))
-			type = IGMP_HOST_MEMBERSHIP_REPORT;
-		else if (IGMP_V2_SEEN(in_dev))
-			type = IGMPV2_HOST_MEMBERSHIP_REPORT;
-		else
-			type = IGMPV3_HOST_MEMBERSHIP_REPORT;
-		igmp_send_report(in_dev, im, type);
-	}
-#endif
-}
 
 /*
  *	A socket has left a multicast group on device dev
